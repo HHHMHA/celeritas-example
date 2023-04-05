d1bef934974cdbb17f73e2a4e6c00df83a1d4c03
J2mF <mp4-12cs5@hotmail.com>
2023-04-05 15:24:54 +0300

    feat: modify home template to add features

--- routes.go
+++ routes.go
@@ -10,6 +10,9 @@ func (a *application) routes() *chi.Mux {
 
 	// add routes here
 	a.App.Routes.Get("/", a.Handlers.Home)
+	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
+	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
+	//a.App.Routes.Get("/sessions", a.Handlers.Home)
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
