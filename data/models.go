c22a0b17a4624a5e809a7ca24be39a15fbbd5f4b
J2mF <mp4-12cs5@hotmail.com>
2023-06-28 11:21:09 +0300

    feat: add db orm

---
+++ data/models.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"database/sql"
+	db2 "github.com/upper/db/v4"
+	"github.com/upper/db/v4/adapter/mysql"
+	"github.com/upper/db/v4/adapter/postgresql"
+	"os"
+)
+
+var db *sql.DB
+
+var upper db2.Session
+
+type Models struct {
+	// any models inserted here and in the New function
+	// are easily accessible throughout the entire application
+}
+
+func New(databasePool *sql.DB) Models {
+	db = databasePool
+
+	if os.Getenv("DATABASE_TYPE") == "mysql" || os.Getenv("DATABASE_TYPE") == "mariadb" {
+		upper, _ = mysql.New(databasePool)
+	} else {
+		// postgresql
+		upper, _ = postgresql.New(databasePool)
+	}
+
+	return Models{}
+}
